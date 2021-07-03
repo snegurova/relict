d35f74f258f9796413a815ef3928ae2455ab3de5
snegurova <snegurova@gmail.com>
2021-07-03 15:22:26 +0300

    fix: animation after letters on safari domcontentloaded

--- relict/js/script.js
+++ relict/js/script.js
@@ -11496,49 +11496,52 @@ radioButtons.forEach((radioButton) => {
 });
 
 // LettersAnimation
-
-const textToAnimate = document.querySelectorAll('._animated-letters');
-const lettersSetsToAnimate = [];
-if (textToAnimate) {
-  textToAnimate.forEach(el => lettersSetsToAnimate.push(el.textContent.split('')));
-}
-
-lettersSetsToAnimate.forEach((set, i) => {
-  textToAnimate[i].innerHTML = '';
-  set.forEach(letter => {
-    if (letter === ' ') {
-      letter = '&nbsp;';
-    }
-    textToAnimate[i].innerHTML += `<span class="_animated-letter">${letter}</span>`
+window.addEventListener('DOMContentLoaded', () => {
+  const textToAnimate = document.querySelectorAll('._animated-letters');
+  const lettersSetsToAnimate = [];
+  if (textToAnimate) {
+    textToAnimate.forEach(el => lettersSetsToAnimate.push(el.textContent.split('')));
+  }
+  
+  lettersSetsToAnimate.forEach((set, i) => {
+    textToAnimate[i].innerHTML = '';
+    set.forEach(letter => {
+      if (letter === ' ') {
+        letter = '&nbsp;';
+      }
+      textToAnimate[i].innerHTML += `<span class="_animated-letter">${letter}</span>`
+    });
   });
+  
+  let char = 0;
+  const chars = document.querySelectorAll('._animated-letter');
+  const animateAfterElements = document.querySelectorAll('._after-letters-animation');
+  
+  if (chars.length > 0) {
+    let timer = setInterval(onTick, 20);
+    
+    function onTick() {
+      const span = chars[char];
+      span.classList.add('_fade-up');
+      char++;
+      if (char === chars.length) {
+        complete();
+        return;
+      }
+    }
+    
+    function complete() {
+      clearInterval(timer);
+      // animateAfterElements.forEach(el => el.classList.add('_active'));
+      for (let i = 0; i < animateAfterElements.length; i++) {
+        const el = animateAfterElements[i];
+        el.classList.add('_active');
+      }
+    }
+  }
 });
 
-let char = 0;
-const chars = document.querySelectorAll('._animated-letter');
-const animateAfterElements = document.querySelectorAll('._after-letters-animation');
 
-if (chars.length > 0) {
-  let timer = setInterval(onTick, 20);
-  
-  function onTick() {
-    const span = chars[char];
-    span.classList.add('_fade-up');
-    char++;
-    if (char === chars.length) {
-      complete();
-      return;
-    }
-  }
-  
-  function complete() {
-    clearInterval(timer);
-    // animateAfterElements.forEach(el => el.classList.add('_active'));
-    for (let i = 0; i < animateAfterElements.length; i++) {
-      const el = animateAfterElements[i];
-      el.classList.add('_active');
-    }
-  }
-}
 
 
 //=================
